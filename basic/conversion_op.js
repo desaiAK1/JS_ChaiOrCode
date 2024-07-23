194a04dc06932e802b0e534b0e29e542da0164be
desaiAK1 <143385638+desaiAK1@users.noreply.github.com>
2024-07-23 03:02:21 +0000

    conversion operations

---
+++ basic/conversion_op.js
@@ -0,0 +1,34 @@
+let  score=33;
+let s="Akash";
+
+console.log(typeof(score));
+console.log(typeof(s));
+
+//converting string to number""
+//"33"-->convert-->33
+// "33aab"-->convert-->NaN
+// true-->1, false-->0
+
+// let valueInNumber=Number(s);
+
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);
+
+
+//boolean conversion.
+
+// 1=>true,0"=>false,
+// ""=>flase; "akash"=>true;
+
+let isLoggedIn=1;
+ 
+let booleanConvert=Boolean(isLoggedIn);
+console.log(booleanConvert);
+
+//string conversion
+
+let anyNumber=3;
+
+let stringConversion=String(anyNumber);
+console.log(stringConversion);
+console.log(typeof(stringConversion));
